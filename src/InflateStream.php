427e9ae66f16771710d8a0ad9f7788a6bb5fe70c
zumba <zumba@iki.fi>
2016-02-14 10:00:25 +0200

    Fix PSR-2 issues

--- src/InflateStream.php
+++ src/InflateStream.php
@@ -22,9 +22,9 @@ class InflateStream implements StreamInterface
     {
         // read the first 10 bytes, ie. gzip header
         $header = $stream->read(10);
-        $filename_header_length = $this->getLengthOfPossibleFilenameHeader($stream, $header);
+        $filenameHeaderLength = $this->getLengthOfPossibleFilenameHeader($stream, $header);
         // Skip the header, that is 10 + length of filename + 1 (nil) bytes
-        $stream = new LimitStream($stream, -1, 10 + $filename_header_length);
+        $stream = new LimitStream($stream, -1, 10 + $filenameHeaderLength);
         $resource = StreamWrapper::getResource($stream);
         stream_filter_append($resource, 'zlib.inflate', STREAM_FILTER_READ);
         $this->stream = new Stream($resource);
@@ -35,16 +35,14 @@ class InflateStream implements StreamInterface
      * @param $header
      * @return int
      */
-    protected function getLengthOfPossibleFilenameHeader(StreamInterface $stream, $header)
+    private function getLengthOfPossibleFilenameHeader(StreamInterface $stream, $header)
     {
         $filename_header_length = 0;
 
-        if( substr(bin2hex($header), 6, 2) === '08' )
-        {
+        if (substr(bin2hex($header), 6, 2) === '08') {
             // we have a filename, read until nil
             $filename_header_length = 1;
-            while( $stream->read(1) !== chr(0) )
-            {
+            while ($stream->read(1) !== chr(0)) {
                 $filename_header_length++;
             }
         }
