981e76557567f8f606e747d1504361ec06821692
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2018-03-18 10:31:01 +0100

    Fix indentation

--- src/Rfc7230.php
+++ src/Rfc7230.php
@@ -4,13 +4,13 @@ namespace GuzzleHttp\Psr7;
 
 final class Rfc7230
 {
-	/**
-	 * Header related regular expressions (copied from amphp/http package)
-	 * (Note: once we require PHP 7.x we could just depend on the upstream package)
-	 *
-	 * @link https://github.com/amphp/http/blob/v1.0.1/src/Rfc7230.php#L12-L15
-	 * @license https://github.com/amphp/http/blob/v1.0.1/LICENSE
-	 */
+    /**
+     * Header related regular expressions (copied from amphp/http package)
+     * (Note: once we require PHP 7.x we could just depend on the upstream package)
+     *
+     * @link    https://github.com/amphp/http/blob/v1.0.1/src/Rfc7230.php#L12-L15
+     * @license https://github.com/amphp/http/blob/v1.0.1/LICENSE
+     */
     const HEADER_REGEX = "(^([^()<>@,;:\\\"/[\]?={}\x01-\x20\x7F]++):[ \t]*+((?:[ \t]*+[\x21-\x7E\x80-\xFF]++)*+)[ \t]*+\r?\n)m";
-	const HEADER_FOLD_REGEX = "(\r?\n[ \t]++)";
+    const HEADER_FOLD_REGEX = "(\r?\n[ \t]++)";
 }
